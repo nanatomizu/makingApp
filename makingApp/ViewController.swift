ebbe11a0005ad870eb0f768d413d5e029a6a9d19
水谷七渡 <mizu710tr@gmail.com>
2018-12-18 14:32:50 +0800

    企画アプリファイルを作りました。

---
+++ makingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  makingApp
+//
+//  Created by 水谷七渡 on 2018/12/18.
+//  Copyright © 2018 Nanato Mizutani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
