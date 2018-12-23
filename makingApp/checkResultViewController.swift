98bdcea5a6880adcf1328e6e268926f4bc6cc972
水谷七渡 <mizu710tr@gmail.com>
2018-12-23 12:49:30 +0800

    recordとrecordCheck間でのデータを送ることに成功しました。

---
+++ makingApp/checkResultViewController.swift
@@ -0,0 +1,59 @@
+//
+//  checkResultViewController.swift
+//  makingApp
+//
+//  Created by 水谷七渡 on 2018/12/22.
+//  Copyright © 2018 Nanato Mizutani. All rights reserved.
+//
+
+import UIKit
+//TODO:もう一枚ページを作って確認するのか、アクションで確認するのかを考える
+//TODO:先ずはデータの送信
+class checkResultViewController: UIViewController {
+    
+    var goals = ["１００キロ走る","フルマラソン完走","美味しいものをたべる"]
+    var rate = ["0%","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"]
+    
+    var comment = ""
+    var goalIndex = -1
+    var rateIndex = -1
+   
+    @IBOutlet weak var checkResultGoalLabel: UILabel!
+    @IBOutlet weak var checkResultCommentTextView: UITextView!
+    @IBOutlet weak var checkResultRateLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(comment)
+        checkResultCommentTextView.text = comment
+
+        // Do any additional setup after loading the view.
+    }
+    //送られてきたデータを表示する
+     override func viewWillAppear(_ animated: Bool) {
+        //前の画面で選択された行番号を表示
+        print("選択された行番号：\(goalIndex)")
+        print("選択された行番号：\(rateIndex)")
+       //ゴールとrateを表示
+        checkResultGoalLabel.text = goals[goalIndex]
+        checkResultRateLabel.text = rate[rateIndex]
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+        
+//        checkResultGoalLabel.text = goals[goalIndex]
+//
+//      checkResultCommentTextView.text =
+//        checkResultRateLabel.text = rate[selectIndex]
+
+}
+}
--- makingApp/recordPageViewController.swift
+++ makingApp/recordPageViewController.swift
@@ -14,6 +14,9 @@ class recordPageViewController: UIViewController,UIPickerViewDataSource,UIPicker
     var goals = ["１００キロ走る","フルマラソン完走","美味しいものをたべる"]
     var rate = ["0%","10%","20%","30%","40%","50%","60%","70%","80%","90%","100%"]
     
+    var goalIndex = -1
+    var rateIndex = -1
+    
     @IBOutlet weak var goalPickerView: UIPickerView!
     @IBOutlet weak var commentTextView: UITextView!
     @IBOutlet weak var ratePickerView: UIPickerView!
@@ -23,8 +26,9 @@ class recordPageViewController: UIViewController,UIPickerViewDataSource,UIPicker
     override func viewDidLoad() {
         super.viewDidLoad()
         goalPickerView.delegate = self
-     
+        goalPickerView.dataSource = self
         ratePickerView.delegate = self
+        ratePickerView.dataSource = self
           goalPickerView.tag = 1
         ratePickerView.tag = 2
         
@@ -42,17 +46,57 @@ class recordPageViewController: UIViewController,UIPickerViewDataSource,UIPicker
             return rate.count
         }
     }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView.tag == 1{
-            print(goals[row])
-            return goals[row]
-        }else{
-            print(rate[row])
-            return rate[row]
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        print("選択されたのは\(row)行目で\(teaList[row])です。 ")
     }
-    
-    
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //行番号を飛ばすのに苦労しました。
+        if pickerView.tag == 1{
+            print("goals[row]:番号\(row)")
+             goalIndex = row
+            return goals[row]
+
+        }else{
+            print("rate[row]:番号\(row)")
+            rateIndex = row
+            return rate[row]
+            
+
+ 
+        }
+        //意味不明なとこ
+        performSegue(withIdentifier:"scRecordResult", sender: nil)
+        
+    }
 
 
-}
+        
+
+        //今選択された行番号を格納
+//        goalIndex = indexPath.row
+//        rateIndex = indexPath.row
+        //セグエの名前を指定して、移動
+
+//    performSegue(withIdentifier:"scRecordResult", sender: nil)
+//        print(indexPath.row)
+
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let crVC = segue.destination as! checkResultViewController
+        crVC.goalIndex = goalIndex
+        crVC.rateIndex = rateIndex
+    
+        if segue.identifier == "scRecordResult"{
+            let checkResultViewController:checkResultViewController = segue.destination as! checkResultViewController
+            checkResultViewController.comment = self.commentTextView.text
+
+        }
+            
+       
+    }
+
+    
+
 }
